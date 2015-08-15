c5a9287995dcd2cbf88a9d7b921866410e853cca
Stefan Bucur <stefan.bucur@epfl.ch>
2015-08-15 22:35:32 +0200

    Chef: Convenience script for building the Chef pass on the qemu VM.

---
+++ chef/llvm-pass/build_pass_i586.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Copyright 2015 EPFL. All rights reserved.
+
+set -x
+set -e
+
+BUILD_DIR=build
+LLVM_DIR=${HOME}/clang+llvm-3.6.2-i586
+
+if [ ! -d ${BUILD_DIR} ]; then
+    mkdir -p ${BUILD_DIR}
+
+    pushd ${BUILD_DIR}
+    cmake \
+	-DLLVM_DIR=${LLVM_DIR}/share/llvm/cmake/ \
+	-DCMAKE_CXX_FLAGS="-march=i586" ..
+    popd
+fi
+
+pushd ${BUILD_DIR}
+make
+popd
